b91be46b49ee2f243021a45b6a8805ab06aa07ee
bastienFouquet <bastienfouquet85@gmail.com>
2019-11-30 11:28:04 +0100

    initial commit

---
+++ app/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public $timestamps = false;
+
+    protected $table = 'user';
+
+    protected $fillable = [
+        'email','firstname', 'lastname', 'password', 'api_token'
+    ];
+
+    public static function users()
+    {
+        return User::all();
+    }
+
+    public function save(array $options = [])
+    {
+        if (is_null($this->api_token)){
+            $this->id = Str::random(32);
+            $this->api_token = Str::random(64);
+        }
+        parent::save($options);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createUser();
+    }
+
+    public function createUser()
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 5; $i++){
+            $user = new \App\User();
+            $user->email = $faker->email;
+            $user->firstname = $faker->firstName;
+            $user->lastname = $faker->lastName;
+            $user->password = $faker->password;
+            $user->save();
+        }
+    }
+}

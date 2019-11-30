a75f20df35efe96daa847eeeea471c7427fac1de
bastienFouquet <bastienfouquet85@gmail.com>
2019-11-30 12:12:20 +0100

    adminlte intergration

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'ReflexYon-API'),
+    'name' => env('APP_NAME', 'ReflexYon'),
 
     /*
     |--------------------------------------------------------------------------
